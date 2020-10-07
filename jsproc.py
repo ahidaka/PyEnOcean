c9489a1ee65316b1c9e05003e420c50f49dfcbdc
ahidaka <hidaka@devdrv.co.jp>
2020-10-07 13:55:11 +0000

    temp save for test

--- jsproc.py
+++ jsproc.py
@@ -4,34 +4,104 @@ import sys
 import json
 
 
-#print('sys.argv         : ', sys.argv)
-#print('type(sys.argv)   : ', type(sys.argv))
-#print('len(sys.argv)    : ', len(sys.argv))
+class Profile():
+    '''
+    EEP management
+    '''
+    def __init__(self, jsfile="D2-14-41.json"): # default file name
+        self.json = open(jsfile, 'r')
+        self.top = json.load(self.json)
 
-print("start")
+        self.outputlist = {}
 
-#i = 0
-#for arg in sys.argv:
-#    print(i)
-#    print(': ', arg)
-#    print(': ', type(arg))
-#    i += 1
+        self.SZ = 8
+        self.SEVEN = 7
+
+    def printall(self):
+        print()
+        print(self.top['EepDefinitions']['profile'])
+        print(self.top['EepDefinitions']['title'])
+        print()
+
+        temp_prof = self.top['functions']['DataName']
+
+        #for v in ["TP", ... ] #function
+        #    print(v)
+
+    def add_outiems(self, item_name, seq):
+        self.outputlist[item_name] = seq
+
+    def operation(self, data):
+        '''
+        Main process to handle data and output
+
+        df['ValueType'],
+        df['DataName'],
+        df['ShortCut'],
+        df['BitOffs'],
+        df['BitSize'],
+        df['RangeMin'],
+        df['RangeMax'],
+        df['ScaleMin'],
+        df['ScaleMax'],
+        df['Unit']
+        '''
+
+        for df in self.datafields:
+            if 'slope' not in df:
+                df['slope'] = self.calc_a(df['RangeMin'], df['ScaleMin'], df['RangeMax'], df['ScaleMax'])
+            if 'offset' not in df:
+                df['offset'] = self.calc_b(df['RangeMin'], df['ScaleMin'], df['RangeMax'], df['ScaleMax'])
+
+            partialData = (int) self.get_bits(data, df['BitOffs'], df['BitSize'];
+
+            if df['ValueType'] == vt_data:
+                value = partialData * slope + offset
+            else:
+                value = partialData;
+
+            if df['ShortCut'] in self.output_list:
+                output(df['ShortCut'], value)
 
 
-json_open = open('sample.json', 'r')
+    def calc_a(self, r_min, s_min, r_max, s_max):
+        return (y1 - y2) / (x1 - x2)
 
-json_load = json.load(json_open)
+    def calc_b(self, r_min, s_min, r_max, s_max):
+        return (x1 * y2 - x2 * y1) / (x1 - x2)
 
-#print(json_load)
+    def get_bits(self, inArray, start, leng):
+        """
+        getbits: How to get some bits from a byte array.
+        """
+        ul = 0
+        startBit = start % self.SZ
+        startByte = start / self.SZ
+        posInArray = int(startByte)
+        dataInArray = 0
+        i = 0
+
+        pos = startBit
+        #for (i = 0; i < leng; i+=1):
+        for i in range(leng):
+            ul <<= 1
+            dataInArray = inArray[posInArray]
+            ul |= (dataInArray >> (self.SEVEN - pos)) & 1
+            pos += 1
+            if pos >= self.SZ:
+                pos = 0
+                posInArray += 1
+        return ul
 
 
-#print(json_load['section1']['key'])
+def main():
+    '''
+    JSProc main
+    '''
+    print("start")
 
-#"EepDefinitions"
-#"profile"
-
-print(json_load['EepDefinitions']['profile'])
-
-for v in json_load.values():
-    print(v)
-    
+    eep = Profile()
+    eep.printall()
+        
+if __name__ == "__main__" :
+    main()
--- pymulti.py
+++ pymulti.py
@@ -5,6 +5,9 @@ import sys
 import json
 
 class Usage():
+    '''
+    Usage print
+    '''
     def __init__(self):
         self.usage_line = [    
             "    usage:",
@@ -51,13 +54,106 @@ class Usage():
             print(line)
 
 
+class SystemControl():
+    '''
+    Analyse start up paarameters and set up at start up
+    '''
+    def __init__(self):
+        self.parameters = {
+            #'u' : 0, # output with unit
+            #'j' : 0, # json output
+            'c' : 1, # CSV output, default
+            #'t' : 0, # Timestamp
+            #'l' : 0, # Learn for TeachIn
+            #'f' : 0, # Filter
+            }
+        self.point_lists = {}
+        self.seq = 0
+
+
+    def setup(self):
+        i = 0
+        for arg in sys.argv:
+            print(i)
+            print(': ', arg)
+            print(': ', type(arg))
+            c = ''
+            option = ''
+
+            if arg[0] == '-' or arg[0] == '+':
+                print("opt<" + arg[0] + "> found") ####
+                oprion = arg[0]
+                c = arg[1]
+                self.set_flag(c, option)
+
+            elif type(arg) is str and arg.endswith(('.json', '.JSON')):
+            #elif arg.endswith('.JSON'):
+                self.json_file = arg
+                print("json<" + arg + "> found") ####
+
+            else:
+                print("other<" + arg + "> found")
+                self.set_point(arg)
+
+            i += 1    
+
+    def set_flag(self, key, option = 1):
+        number = 0
+        if (type(option) is int):
+            number = option
+        elif (option == '+'):
+            number = 1
+        elif (option == '-'):
+            number = -1
+        if number != 0:
+            if key in self.parameters:
+                self.parameters[key] += number
+            else:
+                self.parameters[key] = number
+
+    def get_flag(self, key):
+        val = self.parameters.get(key, "")
+        return val
+
+    def set_point(self, point):
+        self.point_lists[point] = self.seq
+        self.seq += 1
+
+    def get_seq(self, point):
+        val = self.point_lists.get(point, -1)
+        return val
+
+    def print_flagall(self):
+        for k, v in self.parameters.items():
+            print(k, v)
+
+    def print_pointall(self):
+        for k, v in self.point_lists.items():
+            print(k, v)
+
+
 def main():
     '''
     PyMulti main
     '''
+    print('PyMulti...')
 
-    usage = Usage()
-    usage.printall()
+    sc = SystemControl()
+    sc.setup()
+    #usage = Usage()
+    #usage.printall()
+    print()
+
+    print('flags')
+    sc.print_flagall()
+    print()
+
+    print('points')
+    sc.print_pointall()
+    print()
+
+    print('json_file')
+    print(sc.json_file)
     print()
 
 if __name__ == "__main__" :
